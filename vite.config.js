b4a712813a90fabef9914233a0809a0e1423b698
Elias Kaleta <eliaskaleta@gmail.com>
2024-10-03 10:55:05 +1300

    change to work on github pages

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' // If you're using React
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3000',
-    },
+  build: {
+    outDir: 'dist',
   },
 })
